c43691a35c952064b9c9bdcf34f8127a201d0b85
zf <wangzejin1504@gmail.com>
2026-07-01 16:34:33 +0800

    train habitat  video model

--- scripts/infer_ltx_video.sh
+++ scripts/infer_ltx_video.sh
@@ -1,8 +1,8 @@
 bash scripts/infer.sh \
     main.py \
     configs/ltx_model/policy_model_lerobot.yaml \
-    OUTPUTS/ltx/action_video/2026_06_16_17_25_33/step_60000/diffusion_pytorch_model.safetensors \
-    test_result/ltx_action_video \
+    OUTPUTS/ltx/action_only/2026_06_16_15_55_09/step_90000/diffusion_pytorch_model.safetensors \
+    test_result/ltx_action \
     move_to_object \
     29512 \
-    50
+    400
